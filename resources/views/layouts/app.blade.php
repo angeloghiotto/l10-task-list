09bfb8213ddfdd4a9c0c5e232ee66539dbaa8291
Angelo <angeloghiotto@gmail.com>
2023-06-13 14:18:25 -0300

    Adding the first style to layout and index page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,10 +2,11 @@
 <html>
     <head>
         <title>Laravel 10 task list app</title>
+        <script src="https://cdn.tailwindcss.com"></script>
         @yield('styles')
     </head>
-    <body>
-        <h1>@yield('title')</h1>
+    <body class="container mx-auto mt-10 mb-10 max-w-lg">
+        <h1 class="text-2xl mb-4">@yield('title')</h1>
         <div>
             @if (session()->has('success'))
                 <p style="color: green">{{session('success')}}</p>
